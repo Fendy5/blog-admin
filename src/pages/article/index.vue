dfb35a163c2a44b86fa5c01915387b32ebca719e
fendy <zf@fendy5.cn>
2022-05-06 21:56:30 +0800

    feat: ⚡文章管理列表添加分页功能~

--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -1,35 +1,46 @@
 <template>
-  <div class="row">
-    <div v-for="i in articles" :key="i.article_id" class="p-16 col-lg-3 col-md-4 col-sm-6 col-xs-12">
-      <div class="article-card" flat bordered>
-        <q-img class="cover" :src="i.cover"/>
+  <div class="min-w-1200">
+    <div class="row">
+      <div v-for="i in articles" :key="i.article_id" class="p-16 col-lg-3 col-md-4 col-sm-6 col-xs-12">
+        <div class="article-card" flat bordered>
+          <q-img class="cover" :src="i.cover"/>
 
-        <q-card-section>
-          <div class="text-grey">{{ i.updated_at }}</div>
-          <div class="text-h5 q-mt-sm q-mb-xs ellipsis">
+          <q-card-section>
+            <div class="text-grey">{{ i.updated_at }}</div>
+            <div class="text-h5 q-mt-sm q-mb-xs ellipsis">
             <span @click="viewArticle(i.article_id)" class="cursor-pointer">
               {{ i.title }}
             </span>
-          </div>
-          <div class="text-caption text-grey ellipsis-2-lines">
-            {{ i.summary }}
-          </div>
-        </q-card-section>
+            </div>
+            <div class="text-caption text-grey ellipsis-2-lines">
+              {{ i.summary }}
+            </div>
+          </q-card-section>
 
-        <div class="flex justify-between">
-          <div>
-            <q-btn @click="editArticle(i.article_id)" flat color="primary" label="编辑"/>
-            <q-btn @click="deleteArticle(i.article_id)" flat color="negative" label="删除"/>
+          <div class="flex justify-between">
+            <div>
+              <q-btn @click="editArticle(i.article_id)" flat color="primary" label="编辑"/>
+              <q-btn @click="deleteArticle(i.article_id)" flat color="negative" label="删除"/>
+            </div>
+            <p class="view">阅读量：{{ i.view }}</p>
           </div>
-          <p class="view">阅读量：{{ i.view }}</p>
         </div>
       </div>
+    </div>
+    <div class="q-pa-lg flex flex-center">
+      <q-pagination
+        v-model="page.current_page"
+        color="purple"
+        :max="page.last_page"
+        :max-pages="page.per_page"
+        boundary-numbers
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive, ref, watch } from 'vue'
 import { deleteArticleApi, getArticleListApi } from 'src/api/article'
 import { useQuasar } from 'quasar'
 import { useRouter } from 'vue-router'
@@ -48,13 +59,25 @@ export default defineComponent({
     const articles = ref<Article[]>([])
     const $q = useQuasar()
     const $router = useRouter()
+    const page = reactive({
+      per_page: 8,
+      current_page: 1,
+      last_page: 0
+    })
 
     const getList = () => {
-      void getArticleListApi().then(value => {
-        articles.value = value.data.data
+      void getArticleListApi({ page: page.current_page, rowsPerPage: page.per_page }).then(value => {
+        const articleData = value.data
+        articles.value = articleData.data
+        page.last_page = articleData.last_page
       })
     }
     getList()
+
+    watch(() => page.current_page, () => {
+      getList()
+    })
+
     const deleteArticle = (id: string) => {
       $q.dialog({
         title: '警告',
@@ -78,6 +101,7 @@ export default defineComponent({
       window.location.href = `https://blog.fendy5.cn/s/${id}`
     }
     return {
+      page,
       articles,
       deleteArticle,
       editArticle,
@@ -103,5 +127,8 @@ export default defineComponent({
   align-items: center;
   margin-bottom: 0;
   margin-right: 20px;
+}
+.min-w-1200 {
+  min-width: 1200px;
 }
 </style>
